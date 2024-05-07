b9e1792747183cc48e5c0d25303db735de4a47e7
Mohamed charaf Bouderraoui <mbouderr@c2r2p8.1337.ma>
2024-05-07 07:43:23 +0100

    mbouderr.42.fr done + user created sexefully + need stikage for submittion

--- incep/srcs/requirements/mariadb/script.sh
+++ incep/srcs/requirements/mariadb/script.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
-set -x
 
 # Ensure /run/mysqld directory exists
 if [ ! -d "/run/mysqld" ]; then
     mkdir /run/mysqld
 fi
 
-# Generate SQL script
+MADB_ROOT_PASSWORD=$(grep -oP '^MADB_ROOT_PASSWORD=\K.*' /run/secrets/maria_password)
+MADB_PASSWORD=$(grep -oP '^MADB_PASSWORD=\K.*' /run/secrets/maria_password)
 cat << EOF > /tmp/wp.sql
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS ${MADB_NAME};
 CREATE USER IF NOT EXISTS '${MADB_USER}'@'%' IDENTIFIED BY '${MADB_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MADB_NAME}.* TO '${MADB_USER}'@'%';
+GRANT ALL PRIVILEGES ON ${MADB_NAME}.* TO '${MADB_USER}'@'%' IDENTIFIED BY '${MADB_PASSWORD}';
 FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MADB_ROOT_PASSWORD}';
 EOF
 
 # Execute SQL script in bootstrap mode
